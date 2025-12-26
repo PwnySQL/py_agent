2d0c59fee509c905e0f49851d710548a1c35107d
PwnySQL <37075657+PwnySQL@users.noreply.github.com>
2025-12-26 17:50:42 +0100

    ch1l3: get first response from gemini

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+
 def main():
-    print("Hello from py-agent!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if api_key is None:
+        raise RuntimeError("Cannot load gemini api key!")
+    client = genai.Client(api_key=api_key)
+    resp = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+    )
+    print(f"Response: {resp.text}")
 
 
 if __name__ == "__main__":
