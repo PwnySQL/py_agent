40bc8421e0600b84efba8de7a7bbed3224819175
PwnySQL <37075657+PwnySQL@users.noreply.github.com>
2025-12-26 18:14:18 +0100

    chore: refactor l3 and l4 into functions

--- main.py
+++ main.py
@@ -3,23 +3,37 @@ from dotenv import load_dotenv
 from google import genai
 
 
-def main():
+def load_api_key() -> str:
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if api_key is None:
         raise RuntimeError("Cannot load gemini api key!")
+    return api_key
+
+
+def query_gemini(content: str, *, api_key=""):
     client = genai.Client(api_key=api_key)
     resp = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        contents=content,
     )
     if resp.usage_metadata is None:
         raise RuntimeError("Cannot access gemini response metadata! API call failed")
-    print(
-        f"Prompt tokens: {resp.usage_metadata.prompt_token_count}\n"
-        f"Response tokens: {resp.usage_metadata.candidates_token_count}"
+    usage_metadata = resp.usage_metadata
+    return usage_metadata, resp.text
+
+
+def main():
+    api_key = load_api_key()
+    usage_metadata, response = query_gemini(
+        "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        api_key=api_key,
     )
-    print(f"Response: {resp.text}")
+    print(
+        f"Prompt tokens: {usage_metadata.prompt_token_count}\n"
+        f"Response tokens: {usage_metadata.candidates_token_count}"
+    )
+    print(f"Response: {response}")
 
 
 if __name__ == "__main__":
